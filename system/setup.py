22b04551ec20dc3eb2a73f4a68746164a223facd
engag1ng <131041909+engag1ng@users.noreply.github.com>
2023-05-20 15:27:15 +0200

    Add files via upload

---
+++ system/setup.py
@@ -0,0 +1,52 @@
+import sys
+import subprocess
+import os
+import time
+
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'customtkinter'])
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'pyotp'])
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'qrcode'])
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'cryptography'])
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'pyinstaller'])
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'pillow'])
+
+time.sleep(2)
+import pyotp
+id = pyotp.random_base32() #Generate new TOTP Key
+with open('KeyPass.py', 'a') as keypass:
+    line_to_replace = 11 #the line we want to remplace
+    my_file = 'KeyPass.py'
+
+    with open(my_file, 'r') as file:
+        lines = file.readlines()
+
+    if len(lines) > int(line_to_replace):
+        lines[line_to_replace] = "totpkey='"+id+"'"+'\n'
+
+    with open(my_file, 'w') as file:
+        file.writelines( lines )
+
+restore_code = pyotp.random_base32()
+with open('KeyPass.py', 'a') as restore:
+    line_to_replace = 12 #the line we want to remplace
+    my_file = 'KeyPass.py'
+
+    with open(my_file, 'r') as file:
+        lines = file.readlines()
+
+    if len(lines) > int(line_to_replace):
+        lines[line_to_replace] = "restore_code='"+restore_code+"'"+'\n'
+
+    with open(my_file, 'w') as file:
+        file.writelines( lines )
+
+def get_user():
+    return os.getenv('USER', os.getenv('USERNAME', 'user'))
+username = str(get_user())
+
+subprocess.check_call([sys.executable, '-m', 'pip', 'show', 'customtkinter'])
+customtkinter = input("Please input the Location shown above:\n")
+
+#subprocess.run([sys.executable, 'pyinstaller', '--noconfirm', '--onedir', '--windowed', '--add-data','"{location}\customtkinter;customtkinter"', '--icon=keypassicon.ico', 'KeyPass.py'])
+subprocess.run(f'PyInstaller --noconfirm --onedir --windowed --add-data "{customtkinter}\customtkinter;customtkinter" --icon=keypassicon.ico KeyPass.py')
+os.remove('KeyPass.py')
